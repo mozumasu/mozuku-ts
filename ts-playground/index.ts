d80b307682a4737000f61a674d5103e04e048a39
mozumasu <ouri1229@gmail.com>
2026-05-17 17:06:52 +0900

    feat: Implement task management features with add, complete, and print

---
+++ ts-playground/index.ts
@@ -0,0 +1,46 @@
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
+
+const tasks: Task[] = [];
+let id = 0;
+
+// タスクを追加する
+const addTask = (title: string): void => {
+  let incrementedId = ++id;
+  const task = { id: incrementedId, title: title, isComplete: false };
+  tasks.push(task);
+  console.log(
+    ["タスクを追加したにょ", `[ ]: ${task.id} ${task.title}`].join("\n"),
+  );
+  console.log(tasks);
+};
+
+// タスクを表示する
+const printTask = (): void => {
+  console.log("<タスクリスト>");
+
+  for (const task of tasks) {
+    const mark = task.isComplete ? "✔" : " ";
+    console.log(`[${mark}]: ${task.id} ${task.title}`);
+  }
+};
+
+// タスクを完了する
+const completeTask = (id: number): void => {
+  const completeTaskId = id
+  const task = tasks.find((t) => t.id === completeTaskId)
+  if (task != undefined) {
+    task.isComplete = true
+  } else {
+    console.log("その番号のタスクは無いにょ")
+  }
+}
+
+// Todoを使う
+addTask("朝御飯を食べる");
+completeTask(1)
+addTask("昼御飯を食べる");
+printTask();
